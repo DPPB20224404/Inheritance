e8d8c3d40e14e55f5f2c880b766c6c6dc525501f
patricktelnoni <patricktelnoni@users.noreply.github.com>
2022-03-17 07:37:57 +0000

    Tambah Catatan VMI

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
-    Parent anak  = new Child();
-    
+    Parent anak  = new Child(); // Contoh pembuatan object yang mentrigger Virtual Method Invocation
+    Child anak2 = new Child();
     System.out.println("Umur adalah : " + anak.umur);
     anak.jalan();
   }
